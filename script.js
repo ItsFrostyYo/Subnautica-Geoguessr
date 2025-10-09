0f98984fb0964a0da5aa2a572f14c3a1ebd76c5d
ItsFrostyYo <kaleb.austin0009@gmail.com>
2025-10-08 20:15:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const map = L.map("map", {
 });
 
 const mapBounds = [[0, 0], [4096, 4096]];
-L.imageOverlay("./images/Map.png", mapBounds).addTo(map);
+L.imageOverlay("images/Map.png", mapBounds).addTo(map);
 
 map.fitBounds(mapBounds);
 map.setMaxBounds(mapBounds);
@@ -201,4 +201,5 @@ function restartGame() {
   newRound();
   startTotalTimer();
 }
+
 
